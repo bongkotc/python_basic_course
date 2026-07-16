ba492cbbbaadd2f77f223530eb355390fa576158
bongkotc <bongkot.chai@gmail.com>
2026-07-16 18:35:23 +0700

    Create ep6_tuple_set.py

---
+++ ep6_tuple_set.py
@@ -0,0 +1,16 @@
+print("ส่วนที่ 1 Tuple")
+coordinates = (13.7563, 100.5018) #พิกัดละติจูด และลองติจูด
+months = ("มกราคม", "กุมภาพันธ์", "มีนาคม")
+
+print("ข้อมูลใน coordinates: ", coordinates)
+print("ข้อมูลเดือน: ", months[1])
+
+# coordinates[0] = 14.1234 #ลองแก้ไขค่า
+
+print("ส่วนที่ 2 Set")
+fruits = {"apple", "banana" , "orange" , "apple"}
+print("ข้อมูลใน Set ของ fruits: ", fruits)
+fruits.add("mango")
+print("เพิ่มข้อมูลใน Set ของ fruits: ", fruits)
+fruits.remove("banana")
+print("ลงข้อมูลใน Set ของ fruits: ", fruits)
